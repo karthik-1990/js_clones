dc13566fc4fb50f4bbef5d79a56eaa5e2ec9dfb4
karthik-1990 <95838705+karthik-1990@users.noreply.github.com>
2023-07-23 22:34:52 +0530

    Committing the first modal for create store

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import {useEffect} from "react"
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetUpPage = () =>{
+  const onOpen=useStoreModal((state)=>state.onOpen);
+  const isOpen=useStoreModal((state)=>state.isOpen);
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen])
+    return (
+        <div className="p-4">
+          Root Page
+      </div>
+    );
+  }
+  
+export default SetUpPage;
